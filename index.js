e335546a0869dbbed131ec008215e07043b02ed2
pharkasbence <mail.bence.farkas@gmail.com>
2019-04-02 17:25:57 +0200

    refactor error handling

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ import getRequestTypes from './src/requestTypes'
 
 function install(Vue, options = {}) {
     if ( ! options.store ) {
-        console.error('Pending request tracker: Please provide a store!');
+        throw new Error('Pending request tracker: Please provide a store!');
     }
 
     if ( ! options.axios ) {
-        console.error('Pending request tracker: Please provide an axios instance!');
+        throw new Error('Pending request tracker: Please provide an axios instance!');
     }
 
     let registeredRequests = getRequestTypes(options.endpoints);
 
-    if (Object.keys(registeredRequests).length > 0) {
-        let vuexNamespace = (typeof options.vuexNamespace === 'undefined') ?
-        packageConfig.vuexNamespace : options.vuexNamespace;
-
-        options.store.registerModule(vuexNamespace, storeSetup(registeredRequests));
-
-        interceptorSetup(registeredRequests, options.axios, options.store, vuexNamespace);
-    } else {
-        console.error('Pending request tracker: Please provide some endpoints!');
+    if ( Object.keys(registeredRequests).length === 0 ) {
+        throw new Error('Pending request tracker: Please provide some endpoints!');
     }
+
+    let vuexNamespace = (typeof options.vuexNamespace === 'undefined') ?
+    packageConfig.vuexNamespace : options.vuexNamespace;
+
+    options.store.registerModule(vuexNamespace, storeSetup(registeredRequests));
+
+    interceptorSetup(registeredRequests, options.axios, options.store, vuexNamespace);
 }
 
 export default install;
